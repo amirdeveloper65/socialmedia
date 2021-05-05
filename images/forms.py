3abef69644eb3d1e55d1dfc21199052feb8d4c53
amir <amir.hj65@gmail.com>
2021-05-05 20:04:00 +0430

    bookmark image

--- images/forms.py
+++ images/forms.py
@@ -3,11 +3,14 @@ from django.db.models import fields
 from django.forms import widgets
 from django.forms.widgets import Widget
 from .models import Image
- 
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = ('tite','url','description')
+        fields = ('title','url','description')
         widgets = {
             'url':forms.HiddenInput
         }
@@ -20,3 +23,16 @@ class ImageCreateForm(forms.ModelForm):
                 raise forms.ValidationError('The given URL does not ' \
                     'match valid image extensions.')
             return url        
+
+        def save(self, force_insert = False , force_update = False , commit = True ):
+            image = super().save(commit=False)
+            image_url = self.cleaned_data['url'] 
+            name = slugify(image.title)
+            extension = image_url.rsplit('.',1)[1].lower()
+            image_name = f'{name}.{extension}'
+            response = request.urlopen(image_url)
+            image.image.save(image_name,ContentFile(response.read()),save=False)
+
+            if commit:
+                image.save()
+            return image       
--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.db.models.deletion import CASCADE
+from django.utils.text import slugify
 
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='images_created',on_delete=models.CASCADE)
@@ -18,9 +19,7 @@ class Image(models.Model):
     def save(self , *args , **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-        super().save(*args , **kwargs)    
-
-
+        super().save(*args , **kwargs)
 
 
 
